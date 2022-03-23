c06832f00b1481743625022a6e2244d99dfb7070
Jon Courtier <jonaqwerty@yahoo.com>
2022-03-22 22:43:55 -0400

    proper imports

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import About from 'About.js';
-import Skills from 'Skills.js';
-import Projects from 'Projects.js';
-import Links from 'Links.js';
+import About from './About.js';
+import Skills from './Skills.js';
+import Projects from './Projects.js';
+import Links from './Links.js';
 
 export default function Main() {
   return (
