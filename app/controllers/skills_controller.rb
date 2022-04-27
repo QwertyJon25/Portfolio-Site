2933b4c9158b216f0a456e87d722e6bb798d4c26
Jon Courtier <jonaqwerty@yahoo.com>
2022-04-27 19:33:50 -0400

    skills controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,15 @@
 class SkillsController < ApplicationController
+    def index
+        skills = Skill.all
+        render json: skills status: :ok
+    end
+
+    def show
+        skill = Skill.find_by(id: params[:id])
+        if skill
+            render json: skill, status: :ok
+        else
+            render json: { error: "Skill not found" }, status: :not_found
+        end
+    end
 end
