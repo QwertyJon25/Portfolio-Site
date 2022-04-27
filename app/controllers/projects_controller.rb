18747a424e31c9897c977dcb2ca88526de4039ce
Jon Courtier <jonaqwerty@yahoo.com>
2022-04-27 19:32:30 -0400

    projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,15 @@
 class ProjectsController < ApplicationController
+    def index
+        projects = Project.all
+        render json: projects, status: :ok
+    end
+
+    def show
+        project = Project.find_by(id: params[:id])
+        if project
+            render json: project, status: :ok
+        else
+            render json: { error: "Project not found" }, status: :not_found
+        end
+    end
 end
