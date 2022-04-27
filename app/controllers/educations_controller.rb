be5c9889ab3bceb8c31e6455450d1dccb26b6fb7
Jon Courtier <jonaqwerty@yahoo.com>
2022-04-27 19:27:22 -0400

    education controller

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,2 +1,15 @@
 class EducationsController < ApplicationController
+    def index
+        educations = Education.all
+        render json: educations, status: :ok
+    end
+
+    def show
+        education = Education.find_by(id: params[:id])
+        if education
+            render json: education, status: :ok
+        else
+            render json: { error: "Education not found" }, status: :not_found
+        end
+    end
 end
