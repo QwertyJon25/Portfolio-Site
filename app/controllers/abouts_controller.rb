8d9e694b1938dff7eb70a9da78e1c2c58686b73d
Jon Courtier <jonaqwerty@yahoo.com>
2022-04-27 19:23:43 -0400

    about controller

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,2 +1,15 @@
 class AboutsController < ApplicationController
+    def index
+        abouts = About.all
+        render json: abouts, status: :ok
+    end
+
+    def show
+        about = About.find_by(id: params[:id])
+        if about
+            render json: about, status: :ok
+        else
+            render json: { error: "About not found" }, status: :not_found
+        end
+    end
 end
