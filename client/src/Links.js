4a1a3095e92aa6e8249d1d280f43769c626e6b96
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-04 13:54:32 -0400

    styling

--- client/src/Links.js
+++ client/src/Links.js
@@ -4,9 +4,9 @@ export default function Links() {
   return (
     <div className="Links">
             <h2>Links</h2>
-            <a className="footer-link" href="https://www.instagram.com/qwertyjon25/">Instagram</a>
-            <a className="footer-link" href="https://www.linkedin.com/in/jon-courtier/">LinkedIn</a>
-            <a className="footer-link" href="https://github.com/QwertyJon25">GitHub</a>
+            <a className="footer-link" href="https://www.instagram.com/qwertyjon25/">Instagram</a><br/>
+            <a className="footer-link" href="https://www.linkedin.com/in/jon-courtier/">LinkedIn</a><br/>
+            <a className="footer-link" href="https://github.com/QwertyJon25">GitHub</a><br/>
             <p className="copyright">© 2022 Jon Courtier.</p>
         </div>
   )
