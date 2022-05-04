af18340ee9f0223a439cf729b8699f2ba0bbb8de
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-04 13:42:08 -0400

    further link component

--- client/src/Links.js
+++ client/src/Links.js
@@ -4,8 +4,8 @@ export default function Links() {
   return (
     <div className="Links">
             <h2>Links</h2>
-            <p>LinkedIn</p><link href="main.css" rel="stylesheet"/>
-            <p>Github</p><link href="https://github.com/QwertyJon25" rel="stylesheet"/>
+            <p>LinkedIn</p><link href="https://www.linkedin.com/in/jon-courtier/" rel="stylesheet" text="LinkedIn"/>
+            <p>Github</p><link href="https://github.com/QwertyJon25" rel="stylesheet" text="GitHub"/>
         </div>
   )
 }
