cc593de6360753aa602f8ace0a26aeb5173fda2f
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-12 14:01:02 -0400

    continuing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "🌱 Seeding data..."
 
 # Seed your database here
 puts "Creating projects..."
-Project.create(name: "Canine Crushes", desc: "Created with React.js as a frontend framework, Active Record as an ORM, and Sinatra as a backend framework. Featuring React Router and CSS for styling. This application was my third semester project during my time at Flatiron School. Users can view a list of dogs rendered from associated data from a custom backend database, and all entries feature full CRUD capability.", git: "https://github.com/QwertyJon25/Canine-Crushes-frontend", vid: "")
+Project.create(name: "Canine Crushes", desc: "Created with React.js as a frontend framework, Active Record as an ORM, and Sinatra as a backend framework. Featuring React Router and CSS for styling. This application was my third semester project during my time at Flatiron School. Users can view a list of dogs rendered from associated data from a custom backend database, and all entries feature full CRUD capability.", git: "https://github.com/QwertyJon25/Canine-Crushes-frontend", vid: "In Progress")
 
 Project.create(name: "", desc: "", git: "", vid: "")
 
