a2dae1d165ad41d83fdb0dc8a302a489aebd3dbe
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-19 15:44:14 -0400

    last daily

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,6 @@ Skill.create(name: "Postgresql", is_web: "true")
 
 Skill.create(name: "", is_web: "")
 Skill.create(name: "", is_web: "")
-Skill.create(name: "", is_web: "")
 
 Skill.create(name: "Adobe Photoshop", is_web: "false")
 
