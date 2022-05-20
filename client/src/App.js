95ada6f79da58d0ccade2eff2f4c8e21e4a870ab
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-20 03:02:07 -0400

    setting up router play

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,25 @@ function App() {
   return (
     <div className="App">
       <Main />
+      {/* <WelcomePage/>
+    <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Main />}>
+          <Route path="/welcome" element={<WelcomeHeader />} />
+          <Route path="artists" element={<ArtistPage />} />
+          <Route path="directors" element={<DirectorPage/>} />
+          <Route path="galleries" element={<GalleryPage/>} />
+          <Route path="pieces" element={<PiecePage/>} />
+          <Route path="your-pieces" element={<ArtistProfile/>} />
+          <Route path="*" element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+    />
+       </Route>
+     </Routes>
+   </BrowserRouter> */}
     </div>
   );
 }
