1aa92f14f917fe219c75501b912233c0d91664e2
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-21 16:39:30 -0400

    about container

--- client/src/About.js
+++ client/src/About.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react';
 
 export default function About() {
+const [abouts, setAbouts] = useState([])
+
+
+    useEffect(() => {
+        fetch('/abouts/')
+        .then(resp => resp.json())
+        .then(aboutData => setAbouts(aboutData))
+    }, [])
+
+
+const aboutCards = sortedAbouts?.map(aboutObj => <AboutList key={aboutObj.id} aboutData={aboutObj} />)
+
+
   return (
         <div className="About">
+          <ul className="cards">{aboutCards}</ul>
           <h2>About</h2>
             <h3>Howdy.</h3>
                 <p className="intro">I am a Comic-Book Artist, Junior Software Engineer and Full-Stack Web Developer. I work at a bookstore, have held a
