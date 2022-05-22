0a8de354720d83a6ca8a0afb9d979b84582caf84
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-22 13:38:54 -0400

    project component foundation

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import React, {useEffect, useState } from 'react';
 
 export default function Projects() {
+
+const [projects, setProjects] = useState([])
+
+
+    useEffect(() => {
+        fetch('/projects/')
+        .then(resp => resp.json())
+        .then(projectData => setProjects(projectData))
+    }, [])
+
+
+const projectCards = sortedProjects?.map(projectObj => <ProjectList key={projectObj.id} projectData={projectObj} />)
+
+
   return (
     <div className="Projects">
             <h2>Projects</h2>
