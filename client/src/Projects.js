f4238c81381f8967221fe969f0d23ddd6e8d5ee7
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-22 13:39:26 -0400

    adjusting project component

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import React, {useEffect, useState } from 'react';
+// import React, {useEffect, useState } from 'react';
 
 export default function Projects() {
 
-const [projects, setProjects] = useState([])
+// const [projects, setProjects] = useState([])
 
 
-    useEffect(() => {
-        fetch('/projects/')
-        .then(resp => resp.json())
-        .then(projectData => setProjects(projectData))
-    }, [])
+//     useEffect(() => {
+//         fetch('/projects/')
+//         .then(resp => resp.json())
+//         .then(projectData => setProjects(projectData))
+//     }, [])
 
 
-const projectCards = sortedProjects?.map(projectObj => <ProjectList key={projectObj.id} projectData={projectObj} />)
+// const projectCards = sortedProjects?.map(projectObj => <ProjectList key={projectObj.id} projectData={projectObj} />)
 
 
   return (
