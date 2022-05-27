2ca7d287b3f58bf75222edce5cb2d0608922bf2d
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-27 01:29:40 -0400

    form

--- client/src/Skills.js
+++ client/src/Skills.js
@@ -21,6 +21,7 @@ export default function Skills() {
             <h3>Design + Development</h3>
                     <p>I may be green in the realms of coding and programming, but I'm a hard worker and a fast learner.
                         <em>Who</em> doesn't enjoy that combination!?</p>
+                        <p>Tech</p>
             <h3>Illustrator</h3>
                     <p>I've been drawing since I was six years old and have been making comics ever since. While largely
                         self-taught, I
