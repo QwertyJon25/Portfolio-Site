44cc081444d718cef048228b8d9033f7139097f3
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-27 01:30:59 -0400

    working on show form

--- client/src/Skills.js
+++ client/src/Skills.js
@@ -21,7 +21,7 @@ export default function Skills() {
             <h3>Design + Development</h3>
                     <p>I may be green in the realms of coding and programming, but I'm a hard worker and a fast learner.
                         <em>Who</em> doesn't enjoy that combination!?</p>
-                        <p>Tech</p>
+                        {/* <p>Tech</p> <p>Art</p> going to feature drop down for both, iterated to front with boolean filter contingent on is tech or not. */}
             <h3>Illustrator</h3>
                     <p>I've been drawing since I was six years old and have been making comics ever since. While largely
                         self-taught, I
