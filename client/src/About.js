61129bf3fb7cade55a67424863f1f9b8dce5ce91
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-28 22:53:15 -0400

    abouts sort function

--- client/src/About.js
+++ client/src/About.js
@@ -1,25 +1,26 @@
-import React from 'react'
-// import React, {useEffect, useState } from 'react';
+import React, {useEffect, useState } from 'react';
+import AboutList from './AboutList.js';
+
 
 export default function About() {
-// const [abouts, setAbouts] = useState([])
+const [abouts, setAbouts] = useState([])
 
 
-//     useEffect(() => {
-//         fetch('/abouts/')
-//         .then(resp => resp.json())
-//         .then(aboutData => setAbouts(aboutData))
-//     }, [])
+    useEffect(() => {
+        fetch('/abouts/')
+        .then(resp => resp.json())
+        .then(aboutData => setAbouts(aboutData))
+    }, [])
 
 
-// const aboutCards = sortedAbouts?.map(aboutObj => <AboutList key={aboutObj.id} aboutData={aboutObj} />)
+const aboutCards = abouts?.map(aboutObj => <AboutList key={aboutObj.id} aboutData={aboutObj} />)
 
 
   return (
         <div className="About">
-          {/* <ul className="cards">{aboutCards}</ul> */}
           <h2>About</h2>
             <h3>Howdy.</h3>
+            <ul className="aboutList"><AboutList/></ul>
                 <p className="intro">I am a Comic-Book Artist, Junior Software Engineer and Full-Stack Web Developer. I work at a bookstore, have held a
                     varied number of jobs during my time in the workforce, and am an enthusiastic linguistics nerd. I ❤️
                     coffee and enjoy a mostly vegetarian diet.</p>
