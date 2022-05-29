ea68645ba90c1a05992486e032a9d54d460b8e3e
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-28 22:55:44 -0400

    about list assignment

--- client/src/About.js
+++ client/src/About.js
@@ -20,7 +20,7 @@ const aboutCards = abouts?.map(aboutObj => <AboutList key={aboutObj.id} aboutDat
         <div className="About">
           <h2>About</h2>
             <h3>Howdy.</h3>
-            <ul className="cards">{artistCards}</ul>
+            <ul className="aboutCards">{aboutCards}</ul>
                 <p className="intro">I am a Comic-Book Artist, Junior Software Engineer and Full-Stack Web Developer. I work at a bookstore, have held a
                     varied number of jobs during my time in the workforce, and am an enthusiastic linguistics nerd. I ❤️
                     coffee and enjoy a mostly vegetarian diet.</p>
