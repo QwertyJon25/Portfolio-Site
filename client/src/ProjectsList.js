c05a2cb2eb9e6116e6afe2b2e833371e97f8d551
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-12 17:30:19 -0400

    setting up project page

--- client/src/ProjectsList.js
+++ client/src/ProjectsList.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function ProjectsList() {
+export default function ProjectsList({projectData}) {
+  const {name, desc, git, vid} = projectData
+
+
   return (
-    <div>ProjectsList</div>
+    <div>ProjectsList
+      {name}<br/>
+      {desc}<br/>
+    </div>
   )
 }
