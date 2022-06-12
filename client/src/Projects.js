d90a824f0f0f91ff036f2feb255371a6d99a2ce7
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-12 17:31:50 -0400

    cleaning up project component pseudo code

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import ProjectList from './ProjectsList.js'
-// import React, {useEffect, useState } from 'react';
 
 export default function Projects() {
 
   const [projects, setProjects] = useState([])
 
-// const [projects, setProjects] = useState([])
 
       useEffect(() => {
         fetch('/projects/')
@@ -15,14 +13,7 @@ export default function Projects() {
       }, [])
 
 
-//     useEffect(() => {
-//         fetch('/projects/')
-//         .then(resp => resp.json())
-//         .then(projectData => setProjects(projectData))
-//     }, [])
 
-
-// const projectCards = sortedProjects?.map(projectObj => <ProjectList key={projectObj.id} projectData={projectObj} />)
       const projectCards = projects?.map(projectObj => <ProjectList key={projectObj.id} projectData={projectObj} />)
 
 
