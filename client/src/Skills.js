ced6b0bab04e46e85ce1b1cf7a31c67101e9af1a
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-12 17:49:35 -0400

    logic

--- client/src/Skills.js
+++ client/src/Skills.js
@@ -16,15 +16,15 @@ const [showForm, setShowForm] = useState([])
 
 const skillCards = skills?.map(skillObj => <SkillsList key={skillObj.id} skillData={skillObj} />)
 
+
   return (
     <div className="Skills">
           <h2>Skills</h2>
             <h3>Design + Development</h3>
-                    <p>I may be green in the realms of coding and programming, but I'm a hard worker and a fast learner.
+                    <p>I may be green in the realms of coding and programming, but I'm a hard worker and a fast learner. 
                         <em>Who</em> doesn't enjoy that combination!?</p> <br/>
                         <button onClick={() => setShowForm(!showForm)} className="skill-button" style={{backgroundColor: "#8BF5C7"}}>Skills</button>
                         { showForm ? null : <ul className="skill-cards">{skillCards}</ul> }<br/>
-                        {/* going to feature drop down for both, iterated to front with boolean filter contingent on is tech or not.} */}
             <h3>Illustrator</h3>
                       <p>Art</p>
                     <p>I've been drawing since I was six years old and have been making comics ever since. While largely
