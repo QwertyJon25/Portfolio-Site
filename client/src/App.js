5a01bb9c1dca3d926c1441c4e7511218c7ab9c08
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-12 21:52:48 -0400

    rejoice, routing is working!

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       {/* <Main /> */}
-      <Main />
+      {/* <Main /> */}
     <BrowserRouter>
      <Routes>
        <Route path="/" element={<Main />}>
