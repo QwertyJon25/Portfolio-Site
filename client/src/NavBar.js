7e1759e6dfb40b4b781a371a40d4a78883c75662
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-12 20:34:05 -0400

    content for navbar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "darkred",
+    textDecoration: "none",
+    color: "white",
+};
 
 export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+
+    return (
+        <div>
+            <NavLink
+            to="/"
+            exact
+            style={linkStyles}
+            activeStyle={{
+                background: "darkblue",
+            }}
+            >
+                Home
+            </NavLink>
+            <NavLink
+            to="/about"
+            exact
+            style={linkStyles}
+            activeStyle={{
+                background: "darkblue",
+            }}
+            >
+                About
+            </NavLink>
+            <NavLink
+                to="/dog-page"
+                exact
+                style={linkStyles}
+                activeStyle={{
+                    background: "darkblue",
+                }}
+            >
+                Doggies
+            </NavLink>
+            <NavLink
+                to="/agency-page"
+                exact
+                style={linkStyles}
+                activeStyle={{
+                    background: "darkblue",
+                }}
+            >
+                Agency
+            </NavLink>
+        </div>
+    );
 }
