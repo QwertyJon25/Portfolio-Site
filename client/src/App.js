cc346bf7fe80bda749930914f74eb8ddf1d0fc34
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-12 21:43:53 -0400

    troubleshooting client side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Main from './Main.js';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './About.js';
+import Skills from './Skills.js';
+import Projects from './Projects.js';
+import Links from './Links.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      {/* <Main /> */}
       <Main />
-      {/* <Main />
     <BrowserRouter>
      <Routes>
        <Route path="/" element={<Main />}>
           <Route path="/about" element={<About />} />
-          <Route path="skills" element={<ArtistPage />} />
-          <Route path="projects" element={<DirectorPage/>} />
-          <Route path="links" element={<GalleryPage/>} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/links" element={<Links />} />
           <Route path="*" element={
         <main style={{ padding: "1rem" }}>
           <p>There's nothing here!</p>
@@ -22,7 +26,7 @@ function App() {
     />
        </Route>
      </Routes>
-   </BrowserRouter> */}
+   </BrowserRouter>
     </div>
   );
 }
--- client/src/Main.js
+++ client/src/Main.js
@@ -1,30 +1,30 @@
 import React from 'react'
-// import { Outlet, Link } from "react-router-dom";
-import About from './About.js';
-import Skills from './Skills.js';
-import Projects from './Projects.js';
-import Links from './Links.js';
+import { Outlet, Link } from "react-router-dom";
+// import About from './About.js';
+// import Skills from './Skills.js';
+// import Projects from './Projects.js';
+// import Links from './Links.js';
 
 export default function Main() {
   return (
         <div className="Main">
           <img src="https://i.imgur.com/s3X9BIJ.jpg" alt="Jon Courtier" width="300" height="350"/>
-            <About />
+            {/* <About />
             <Skills />
             <Projects />
-            <Links />
-            {/* <nav
+            <Links /> */}
+            <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem"
         }}
       >
-        <Link to="/">About</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
         <Link to="/skills">Skills</Link> |{" "}
         <Link to="/projects">Projects</Link> |{" "}
         <Link to="/links">Links</Link> |{" "}
       </nav>
-      <Outlet /> */}
+      <Outlet />
         </div>
   )
 }
